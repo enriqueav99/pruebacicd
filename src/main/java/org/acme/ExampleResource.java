0cd0a633e0be8a7e73314dc3614bd9cfeb4886ba
Administrador <Administrador@MacBooktrador68.home>
2023-02-16 21:15:14 +0100

    primero

---
+++ src/main/java/org/acme/ExampleResource.java
@@ -0,0 +1,16 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/saludo")
+public class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hola, esto es una prueba de ci cd";
+    }
+}
