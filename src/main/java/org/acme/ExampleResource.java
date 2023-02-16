91a2c472b4ef828bb2eb945fa338fc8ba8540f6c
Administrador <Administrador@MacBooktrador68.home>
2023-02-16 23:59:41 +0100

    passan

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -11,6 +11,6 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hola, esto es una prueba de ci cd, parece que va el ci, y ahora el cd, pespes es satan encarnado";
+        return "Hola, esto es una prueba de ci cd, parece que va el ci, y ahora el cd";
     }
 }
