d7a99a7d813b121eb61f651975ffd8c67e2279e4
Administrador <Administrador@MacBooktrador68.home>
2023-02-20 20:25:44 +0100

    commit

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -11,7 +11,7 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hola buenas tardes, esto es una prueba de ci cd con github actions con cache, " +
+        return "Hola buenas tardes, esto es una prueba de CI/CD con github actions con cache, " +
                 "esto esta desglegado en kuberentes dentro de google cloud. El cd es actualizando el chart de helm.";
     }
 }
